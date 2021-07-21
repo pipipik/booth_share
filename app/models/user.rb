08863278eff1d21b0622d49e143b1451c67cc1a8
pipipik <hiro.konan913@gmail.com>
2021-07-21 11:06:17 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_format_of :password, with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  with_options presence: true do
+    validates :name, :occupation, :position
+  end
+  
 end
