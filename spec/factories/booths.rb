24e23433a706e7f665ceb14c7d33fd134f6486c2
pipipik <hiro.konan913@gmail.com>
2021-08-12 00:17:47 +0900

    テストコード

--- spec/factories/booths.rb
+++ spec/factories/booths.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :booth do
-    
+    prize_name   {Faker::Name.name}
+    machine_name {Faker::Name.name}
+    category     {Faker::Name.name}
+
+    association :user
+
+    after(:build) do |booth|
+      booth.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
 end
--- spec/models/booth_spec.rb
+++ spec/models/booth_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Booth, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @booth = FactoryBot.build(:booth)
+  end
+
+  describe 'ブース投稿' do
+    context 'ブース投稿できるとき' do
+      it 'image, prize_name, machine_name, categoryが存在すれば登録できる' do
+        expect(@booth).to be_valid
+      end
+    end
+    context 'ブース投稿できないとき' do
+      it 'imageが空では登録できない' do
+        @booth.image = nil
+        @booth.valid?
+        expect(@booth.errors.full_messages).to include ("Image can't be blank")
+      end
+      it 'prize_nameが空では登録できない' do
+        @booth.prize_name = ''
+        @booth.valid?
+        expect(@booth.errors.full_messages).to include ("Prize name can't be blank")
+      end
+      it 'machine_nameが空では登録できない' do
+        @booth.machine_name = ''
+        @booth.valid?
+        expect(@booth.errors.full_messages).to include ("Machine name can't be blank")
+      end
+      it 'categoryが空では登録できない' do
+        @booth.category = ''
+        @booth.valid?
+        expect(@booth.errors.full_messages).to include ("Category can't be blank")
+      end
+    end
+  end
 end
