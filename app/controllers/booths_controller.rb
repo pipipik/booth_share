8cdc09a29a2635cfc54c94cf7f541757c58df127
pipipik <hiro.konan913@gmail.com>
2021-08-11 20:08:51 +0900

    投稿を保存

--- app/controllers/booths_controller.rb
+++ app/controllers/booths_controller.rb
@@ -8,4 +8,19 @@ class BoothsController < ApplicationController
     @booth = Booth.new
   end
 
+  def create
+    @booth = Booth.new(booth_params)
+    if @booth.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booth_params
+    params.require(:booth).permit(:image, :prize_name, :machine_name, :category).merge(user_id: current_user.id)
+  end
+
 end
