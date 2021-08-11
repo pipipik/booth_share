2087852e112704334619d9b1b3d1c2c22a79f420
pipipik <hiro.konan913@gmail.com>
2021-08-11 20:12:57 +0900

    バリデーション

--- app/models/booth.rb
+++ app/models/booth.rb
@@ -1,5 +1,9 @@
 class Booth < ApplicationRecord
 
+  with_options presence: true do
+    validates :image, :prize_name, :machine_name, :category
+  end
+
   belongs_to :user
   has_one_attached :image
   
